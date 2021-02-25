32a33108f35c6e449dd3beb513a64136ff4d022e
Amy Lio <amy.lio@yahoo.com>
2021-02-25 03:17:30 -0800

    Add files via upload

--- static/js/challenge_logic.js
+++ static/js/challenge_logic.js
@@ -25,7 +25,7 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 let map = L.map('mapid', {
 	center: [40.7, -94.5],
 	zoom: 3,
-	layers: [streets]
+	layers: [satelliteStreets]
 });
 
 // Create a base layer that holds all three maps.
