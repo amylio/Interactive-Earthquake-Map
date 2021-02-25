86b9c9bd93b0468e2bb06eb2e06ad083f3c18668
Amy Lio <amy.lio@yahoo.com>
2021-02-25 03:03:04 -0800

    Add files via upload

--- static/js/challenge_logic.js
+++ static/js/challenge_logic.js
@@ -5,20 +5,20 @@ console.log("working");
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
-	accessToken: API_KEY
+	accessToken: 'pk.eyJ1IjoiamVsbHk3NjE1IiwiYSI6ImNrbGlkdjdmajA4dDAyeXFyOWJtbHdmMTgifQ.1skpHaKCWclY4PbR4O1D0A'
 });
 
 // We create the second tile layer that will be the background of our map.
 let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
-	accessToken: API_KEY
+	accessToken: 'pk.eyJ1IjoiamVsbHk3NjE1IiwiYSI6ImNrbGlkdjdmajA4dDAyeXFyOWJtbHdmMTgifQ.1skpHaKCWclY4PbR4O1D0A'
 });
 
 let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    accessToken: API_KEY
+    accessToken: 'pk.eyJ1IjoiamVsbHk3NjE1IiwiYSI6ImNrbGlkdjdmajA4dDAyeXFyOWJtbHdmMTgifQ.1skpHaKCWclY4PbR4O1D0A'
 });
 
 // Create the map object with center, zoom level and default layer.
